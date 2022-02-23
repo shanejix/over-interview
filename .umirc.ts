d1e048abd92ff654bf2f273954c4d747d9f65d5c
shanejix <shanejix@hotmail.com>
2022-02-23 20:19:50 +0800

    feat: add dumi site

---
+++ .umirc.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: `ShaneJix's Site`,
+  mode: 'site',
+  favicon: '../assets/favicon.png',
+  logo: '../icons/icon-96x96.png',
+  locales: [
+    // ['zh-CN', '中文'],
+    ['en-US', 'English'],
+  ],
+  publicPath: './',
+  history: { type: 'hash' },
+  // more config: https://d.umijs.org/config
+});
