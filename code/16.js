bd406939569e38ae17b3c853975ee609a349a533
shanejix <shanejix@hotmail.com>
2022-03-27 15:29:12 +0800

    chore: update

--- code/16.js
+++ code/16.js
@@ -9,6 +9,31 @@ function debounce(func, wait) {
 
 }
 
+function debounce2(func, wait, immediate) {
+  let timer = null;
+
+  return function () {
+
+    clearTimeout(timer)
+
+    if (immediate) {
+
+      let callNow = !timer;
+
+      timer = setTimeout(() => func(), wait);
+
+      if (callNow) {
+        func()
+      }
+
+    } else {
+
+      timer = setTimeout(() => func(), wait)
+    }
+  }
+
+}
+
 
 function foo(...args) {
   console.log(1)
@@ -17,8 +42,11 @@ function foo(...args) {
 
 const bar = debounce(foo, 3000);
 
+const bar1 = debounce2(foo, 3000, true);
+
 for (let i = 0; i < 30000; i++) {
-  bar()
+  // bar()
+  bar1()
 }
 
 // for (let i = 0; i < 30000000; i++) {
